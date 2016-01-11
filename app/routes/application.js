3c63b0bdcc6949c023f799e267973151425bb594
ozanmuyes <ozi5169@gmail.com>
2016-01-11 10:48:19 +0200

    Many changes
    * Error handling started
    * Logout button added
    * Some dependencies upgraded

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,4 +2,35 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
+  actions: {
+    error(error, transition) {
+      if (error) {
+        /**
+         * Sample error object
+         *
+         * error = {
+         *  description: undefined,
+         *  fileName: "http://eos.dev/assets/vendor.js",
+         *  lineNumber: 26437,
+         *  message: "Adapter operation failed",
+         *  name: "Error",
+         *  number: undefined,
+         *  stack: "EmberError@http://eos.dev/assets/ve…",
+         *  errors: {
+         *    code:"0x00000023",
+         *    detail:"control exception, code is 35, previous is null",
+         *    meta: {
+         *      file:"/home/vagrant/htdocs/eos-api/app/Http/Controllers/v1/UsersController.php",
+         *      line:36
+         *    },
+         *    status:"500",
+         *    title:"Internal Server Error"
+         *   }
+         * }
+         */
+        console.log(error);
+        //return this.transitionTo('errorPage');
+      }
+    }
+  }
 });
